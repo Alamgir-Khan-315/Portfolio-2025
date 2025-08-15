42c8c19bef2797c6d4187e381528a7fddcf58c64
Alamgir Khan <154860642+Alamgir-Khan-315@users.noreply.github.com>
2025-08-16 03:05:55 +0500

    added new project

--- src/Pages/Project/Projects.jsx
+++ src/Pages/Project/Projects.jsx
@@ -5,9 +5,10 @@ import ProjectCard from "./ProjectCard";
 
 const projectsData = [
   { id: 1, logo:'/img/Projects/rock-paper-scissors.png' , name: 'Rock Paper Scissors', description:'A simple interactive game where players choose rock, paper, or scissors to compete against the computer, with instant results and score tracking.', tags: ['HTML', 'CSS', 'JavaScript'], Giticon: <SlSocialGithub /> , Github:'https://github.com/Alamgir-Khan-315/Rock-Paper-Scissor.git' ,  Liveicon: <GoArrowRight/> , Live:'https://rock-paper-scissor-sigma-one.vercel.app/'},
+  { id: 2, logo:'/img/Projects/AquaFlow.png' , name: 'Aqua Flow', description:'Aquaflow delivers pure, refreshing water directly to your doorstep with a focus on quality, speed, and sustainability.', tags: ['HTML', 'TailwindCSS' , 'JavaScript'], Giticon: <SlSocialGithub /> , Github:'https://github.com/Alamgir-Khan-315/AquaFlow.git' ,  Liveicon: <GoArrowRight/> , Live:'https://aqua-flow-pearl.vercel.app/'},
   { id: 2, logo:'/img/Projects/Company.png' , name: 'Company software', description:'A responsive, user-friendly SaaS dashboard built with modern frontend technologies, featuring real-time data visualization, dynamic forms, and smooth user interactions. Designed to deliver a seamless experience across devices with an intuitive and polished UI.', tags: ['React js', 'TailwindCSS', 'Frontend' ,  'Vite'], Giticon: <SlSocialGithub /> , Github:'https://github.com/Alamgir-Khan-315/Company_Web-S-D-.git' ,  Liveicon: <GoArrowRight/> , Live:''},
   { id: 2, logo:'/img/Projects/Logo.png' , name: 'Todo App', description:'A modern, responsive to-do list app with task completion tracking, animated checkmarks, and real-time task counters. Built with React and Tailwind CSS for a clean, mobile-friendly interface.', tags: ['HTML', 'TailwindCSS' , 'JavaScript'], Giticon: <SlSocialGithub /> , Github:'https://github.com/Alamgir-Khan-315/Todo-App.git' ,  Liveicon: <GoArrowRight/> , Live:'https://todo-app-tau-wine-11.vercel.app/'},
-];
+ ];
 
 export default function Projects() {
   return (
